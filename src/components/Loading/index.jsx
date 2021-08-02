865c444fec53695da8090e4430aad5a03848c306
jooprima <jooprima@gmail.com>
2021-08-02 21:40:24 +0700

    Loading Component

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Loading.module.css";
+
+const Loading = () => {
+  <div className={styles.container}>
+    <div className={styles.loader}></div>
+  </div>;
+};
+
+export default Loading;
